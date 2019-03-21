cf65076aa1f8b8221cdbe8bba9a78988c97cd0a6
james-braun <braun.james.robert@gmail.com>
2019-03-21 01:13:34 -0500

    created player array and seperated players into teams

--- Soccer-League-Coordinator.playground/Contents.swift
+++ Soccer-League-Coordinator.playground/Contents.swift
@@ -6,7 +6,7 @@ let players = [
     ["Name": "Matt Gill", "Height": "40.0", "Experience": "false", "Parent": "Charles and Silvia Gill"],
     ["Name": "Kimmy Stein", "Height": "41.0", "Experience": "false", "Parent": "Bill and Hillary Stein"],
     ["Name": "Sammy Adams", "Height": "45.0", "Experience": "false", "Parent": "Jeff Adams"],
-    ["Name": "Karl Saygan", "Height": "42.0", "Experience": "true", "Parent": "WHeather Bledsoe"],
+    ["Name": "Karl Saygan", "Height": "42.0", "Experience": "true", "Parent": "Heather Bledsoe"],
     ["Name": "Suzane Greenberg", "Height": "44.0", "Experience": "true", "Parent": "Henrietta Dumas"],
     ["Name": "Sal Dall", "Height": "41.0", "Experience": "false", "Parent": "Gala Dall"],
     ["Name": "Joe Kavalier", "Height": "39.0", "Experience": "false", "Parent": "Sam and Elaine Kavalier"],
@@ -23,8 +23,10 @@ let players = [
 
 var teamSharks: [[String: String]] = []
 var teamDragons: [[String: String]] = []
+var teamRaptor: [[String: String]] = []
 var sharkCount = 0
 var dragonCount = 0
+var raptorCount = 0
 var totalCount = 0
 for player in players {
     if player["Experience"]! == "true" && sharkCount <= totalCount {
@@ -33,13 +35,27 @@ for player in players {
     } else if player["Experience"]! == "true" && dragonCount <= totalCount {
         teamDragons.append(player)
         dragonCount += 1
-    } else if dragonCount >= totalCount && sharkCount >= totalCount {
+    } else if player["Experience"]! == "true" && raptorCount <= totalCount {
+        teamRaptor.append(player)
+        raptorCount += 1
+    } else if dragonCount >= totalCount && sharkCount >= totalCount && raptorCount >= totalCount {
+        if player["Experience"]! == "true" {
+            teamSharks.append(player)
+            sharkCount += 1
+        }
         totalCount += 1
     }
 }
+print("shark team")
 for player in teamSharks {
    print(player)
 }
+print("dragon team")
 for player in teamDragons {
     print(player)
 }
+print("raptor team")
+for player in teamRaptor {
+    print(player)
+}
+Int("43")
